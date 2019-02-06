c2550d2bd2fa5d0440591af83e2ff9839db99247
dotdidik <didik.irawann@gmail.com>
2019-02-06 16:28:19 +0700

    reactstrap bootstrap

---
+++ fullstack-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import AppHome from './AppHome'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <AppHome />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ fullstack-app/src/AppHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppHead from './AppHead';
+
+class AppHome extends React.Component {
+      render() {
+          return(
+            <AppHead />
+          )
+      }
+}
+
+export default AppHome;
